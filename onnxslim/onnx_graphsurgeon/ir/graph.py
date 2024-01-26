73a34e52eeaf2f2a726be1ae87b3f1313917a647
inisis <desmond.yao@buaa.edu.cn>
2024-01-26 19:06:49 +0800

    speed up sort

--- onnxslim/onnx_graphsurgeon/ir/graph.py
+++ onnxslim/onnx_graphsurgeon/ir/graph.py
@@ -446,6 +446,10 @@ class Graph(object):
                     "Cycle detected in graph! Are there tensors with duplicate names in the graph?"
                 )
 
+            hierarchy_levels[self._get_node_id(node)] = HierarchyDescriptor(
+                node, level=max_input_level + 1
+            )
+
             return max_input_level + 1
 
         with self.node_ids():
