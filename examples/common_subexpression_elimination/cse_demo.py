de7ac80e22c98b5d67ecedd26f651f2d12341415
inisis <46103969+inisis@users.noreply.github.com>
2024-03-21 00:17:11 +0800

    [Feature] add common_subexpression_elimination doc (#11)

---
+++ examples/common_subexpression_elimination/cse_demo.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        embedding_dim = 10
+        self.layer_norm = nn.LayerNorm(embedding_dim)
+
+    def forward(self, x):
+        return self.layer_norm(x) + F.layer_norm(x, [10])
+
+
+layer_norm = Model()
+
+batch, sentence_length, embedding_dim = 20, 5, 10
+embedding = torch.randn(batch, sentence_length, embedding_dim)
+torch.onnx.export(layer_norm, embedding, "ln_cse.onnx", opset_version=13)
