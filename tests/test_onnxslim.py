d8e50a27322ad23139ac864bb6231e70cf9c3983
inisis <desmond.yao@buaa.edu.cn>
2024-09-01 12:17:48 +0000

    add cmd mode test

--- tests/test_onnxslim.py
+++ tests/test_onnxslim.py
@@ -36,6 +36,13 @@ class TestFeature:
         print_model_info_as_table(request.node.name, summary)
         assert summary["op_input_info"]["input"][1] == (1, 3, 224, 224)
 
+        with tempfile.TemporaryDirectory() as tempdir:
+            output_name = os.path.join(tempdir, "resnet18.onnx")
+            slim(FILENAME, output_name, input_shapes=["input:1,3,224,224"])
+            summary = summarize_model(output_name)
+            print_model_info_as_table(request.node.name, summary)
+            assert summary["op_input_info"]["input"][1] == (1, 3, 224, 224)
+
     def test_fp162fp32_conversion(self, request):
         """Test the conversion of an ONNX model from FP16 to FP32 precision."""
         import numpy as np
@@ -60,13 +67,27 @@ class TestFeature:
         print_model_info_as_table(request.node.name, summary)
         assert "/Flatten_output_0" in summary["op_output_info"]
 
+        with tempfile.TemporaryDirectory() as tempdir:
+            output_name = os.path.join(tempdir, "resnet18.onnx")
+            slim(FILENAME, output_name, outputs=["/Flatten_output_0"])
+            summary = summarize_model(output_name)
+            print_model_info_as_table(request.node.name, summary)
+            assert "/Flatten_output_0" in summary["op_output_info"]
+
     def test_input_modification(self, request):
         """Tests input modification."""
         summary = summarize_model(slim(FILENAME, inputs=["/maxpool/MaxPool_output_0", "/layer1/layer1.0/relu/Relu_output_0"]))
         print_model_info_as_table(request.node.name, summary)
         assert "/maxpool/MaxPool_output_0" in summary["op_input_info"]
         assert "/layer1/layer1.0/relu/Relu_output_0" in summary["op_input_info"]
-        raise
+
+        with tempfile.TemporaryDirectory() as tempdir:
+            output_name = os.path.join(tempdir, "resnet18.onnx")
+            slim(FILENAME, output_name, inputs=["/maxpool/MaxPool_output_0", "/layer1/layer1.0/relu/Relu_output_0"])
+            summary = summarize_model(output_name)
+            print_model_info_as_table(request.node.name, summary)
+            assert "/maxpool/MaxPool_output_0" in summary["op_input_info"]
+            assert "/layer1/layer1.0/relu/Relu_output_0" in summary["op_input_info"]
 
 
 if __name__ == "__main__":
