78318e65a0469a6300f3bf961590c19510c241c0
inisis <desmond.yao@buaa.edu.cn>
2025-09-26 23:44:10 +0800

    fix: symbolic shape inference for shape op (#195)
    * fix: symbolic shape inference for shape op
    
    * fix: incorrect usage of attribute
    
    * Auto-format by https://ultralytics.com/actions
    
    * fix: end is None should return all
    
    * Auto-format by https://ultralytics.com/actions
    
    ---------
    
    Co-authored-by: UltralyticsAssistant <web@ultralytics.com>

--- onnxslim/third_party/symbolic_shape_infer.py
+++ onnxslim/third_party/symbolic_shape_infer.py
@@ -1944,7 +1944,25 @@ class SymbolicShapeInference:
 
     def _infer_Shape(self, node):  # noqa: N802
         """Infers and sets the symbolic shape for the output node in the computation graph."""
-        self.sympy_data_[node.output[0]] = self._get_sympy_shape(node, 0)
+        start = get_attribute(node, "start", 0)
+        end = get_attribute(node, "end", None)
+
+        full_sympy_shape = self._get_sympy_shape(node, 0)
+        num_dims = len(full_sympy_shape)
+
+        if start < 0:
+            start = num_dims + start
+        if end is None:
+            end = num_dims
+        elif end < 0:
+            end = num_dims + end
+
+        assert 0 <= start <= end <= num_dims, (
+            f"reshape start/end invalid: start={start}, end={end}, total_dims={num_dims}"
+        )
+
+        target_sympy_shape = full_sympy_shape[start:end]
+        self.sympy_data_[node.output[0]] = target_sympy_shape
 
     def _infer_Size(self, node):  # noqa: N802
         """Infers and sets the size of the output node by computing the product of its shape in the computation
